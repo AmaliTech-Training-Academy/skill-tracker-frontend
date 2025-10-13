4136a3377d7cc68191f04200e64f49320058f607
Faisal <pros3sa@gmail.com>
2025-10-13 20:06:06 +0000

    chore: add various page components of dashboard

---
+++ src/app/features/dashboard/pages/mainpage/mainpage.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainpage',
+  imports: [],
+  templateUrl: './mainpage.html',
+  styleUrl: './mainpage.scss',
+})
+export class Mainpage {}
---
+++ src/app/features/dashboard/pages/skillarena/skillarena.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skillarena',
+  imports: [],
+  templateUrl: './skillarena.html',
+  styleUrl: './skillarena.scss',
+})
+export class Skillarena {}
