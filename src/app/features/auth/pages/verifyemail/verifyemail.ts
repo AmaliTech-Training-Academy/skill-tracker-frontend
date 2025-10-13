0179d3f0b49ad67a8a1b2f6a96074550aadaf349
Faisal <pros3sa@gmail.com>
2025-10-13 17:06:21 +0000

    chore: add verify email compinent

---
+++ src/app/features/auth/pages/verifyemail/verifyemail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verifyemail',
+  imports: [],
+  templateUrl: './verifyemail.html',
+  styleUrl: './verifyemail.scss',
+})
+export class Verifyemail {}
