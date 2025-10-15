0a2aadf594adc8d1eeaaf29cd0f3c262e3ce5dfe
Faisal <pros3sa@gmail.com>
2025-10-15 17:06:03 +0000

    refactor: change reusable input html to match old input component

--- src/app/shared/input-field/input-field.ts
+++ src/app/shared/input-field/input-field.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, AbstractControl, FormControl } from '@angular/forms';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
@@ -9,49 +9,52 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, FontAwesomeModule],
   template: `
-    <div class="input-field">
+    <div class="login-input--group" *ngIf="type !== 'password'; else passwordField">
       <label [for]="id">{{ label }}</label>
+      <input [type]="type" [id]="id" [formControl]="control" [placeholder]="placeholder" />
 
-      <div class="input-wrapper" [ngSwitch]="type">
-        <!-- Password input -->
-        <ng-container *ngSwitchCase="'password'">
+      <!-- Validation -->
+      <div class="error-message" *ngIf="control.invalid && control.touched">
+        <small *ngIf="control.errors?.['required']">{{ label }} is required.</small>
+        <small *ngIf="control.errors?.['email']">Please enter a valid email.</small>
+        <small *ngIf="control.errors?.['minlength']">
+          {{ label }} must be at least
+          {{ control.errors?.['minlength'].requiredLength }} characters.
+        </small>
+      </div>
+    </div>
+
+    <!-- Password field -->
+    <ng-template #passwordField>
+      <div class="login-inputgroup-password">
+        <label [for]="id">{{ label }}</label>
+        <div class="input-password">
           <input
             [type]="showPassword ? 'text' : 'password'"
             [id]="id"
-            [formControl]="formControl"
+            [formControl]="control"
             [placeholder]="placeholder"
           />
           <button
             type="button"
-            class="toggle-btn"
+            class="password-toggle"
             (click)="togglePasswordVisibility()"
             tabindex="-1"
-            aria-label="Toggle password visibility"
           >
             <fa-icon [icon]="showPassword ? faEyeSlash : faEye"></fa-icon>
           </button>
-        </ng-container>
+        </div>
 
-        <!-- Default input -->
-        <input
-          *ngSwitchDefault
-          [type]="type"
-          [id]="id"
-          [formControl]="formControl"
-          [placeholder]="placeholder"
-        />
+        <!-- Validation -->
+        <div class="error-message" *ngIf="control.invalid && control.touched">
+          <small *ngIf="control.errors?.['required']">{{ label }} is required.</small>
+          <small *ngIf="control.errors?.['minlength']">
+            {{ label }} must be at least
+            {{ control.errors?.['minlength'].requiredLength }} characters.
+          </small>
+        </div>
       </div>
-
-      <!-- Validation -->
-      <div class="error-message" *ngIf="formControl?.invalid && formControl?.touched">
-        <small *ngIf="formControl?.errors?.['required']">{{ label }} is required.</small>
-        <small *ngIf="formControl?.errors?.['email']">Please enter a valid email.</small>
-        <small *ngIf="formControl?.errors?.['minlength']">
-          {{ label }} must be at least
-          {{ formControl?.errors?.['minlength']?.requiredLength }} characters.
-        </small>
-      </div>
-    </div>
+    </ng-template>
   `,
   styleUrls: ['./input-field.scss'],
 })
@@ -60,18 +63,11 @@ export class InputFieldComponent {
   @Input() type: 'text' | 'email' | 'password' = 'text';
   @Input() placeholder = '';
   @Input() id = '';
-
-  // Accept AbstractControl from the parent, which covers FormControl/FormGroup/etc.
-  @Input({ required: true }) control!: AbstractControl<any, any>;
+  @Input({ required: true }) control!: FormControl;
 
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
-
-  // Getter that safely casts control to FormControl for template bindings
-  get formControl(): FormControl {
-    return this.control as FormControl;
-  }
 
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
