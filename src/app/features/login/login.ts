10c6a2ade76bf6c67dc3a53d072db12e7d57bf9d
Faisal <pros3sa@gmail.com>
2025-10-15 09:06:36 +0000

    chore: add base html and style for login page

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss'],
 })
-export class Login {}
+export class Login {
+  successMessage: string = '';
+  errorMessage: string = '';
+  email: string = '';
+  password: string = '';
+
+  login() {
+    // Simulate a login process
+    if (this.email === 'user@example.com' && this.password === 'password') {
+      this.successMessage = 'Login successful!';
+      this.errorMessage = '';
+    } else {
+      this.errorMessage = 'Invalid email or password.';
+      this.successMessage = '';
+    }
+  }
+}
