d01af950f4e04fa1789aa5bcc8eb7a6074ab679a
Faisal <pros3sa@gmail.com>
2025-10-15 10:44:59 +0000

    chore: add validators to input fields

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,22 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faEye as farEye, faEyeSlash as farEyeSlash } from '@fortawesome/free-regular-svg-icons';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, FontAwesomeModule, RouterLink],
   templateUrl: './login.html',
   styleUrls: ['./login.scss'],
 })
 export class Login {
-  successMessage: string = '';
-  errorMessage: string = '';
-  email: string = '';
-  password: string = '';
+  successMessage = '';
+  errorMessage = '';
+  email = '';
+  password = '';
+  showPassword = false;
+
+  faEye = farEye;
+  faEyeSlash = farEyeSlash;
+
+  constructor(private faLibrary: FaIconLibrary) {
+    this.faLibrary.addIcons(faEye, faEyeSlash, farEye, farEyeSlash);
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 
   login() {
-    // Simulate a login process
     if (this.email === 'user@example.com' && this.password === 'password') {
       this.successMessage = 'Login successful!';
       this.errorMessage = '';
