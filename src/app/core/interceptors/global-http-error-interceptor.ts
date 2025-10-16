d096aa593aff939085982d33b4c429a1accb2fb8
Victor Bruce <victorbruce82@gmail.com>
2025-10-16 13:21:27 +0000

    refactor(error-interceptor): remove delay from retry and move logic in tap to catchError

--- src/app/core/interceptors/global-http-error-interceptor.ts
+++ src/app/core/interceptors/global-http-error-interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { catchError, throwError, tap, retry, timer } from 'rxjs';
+import { catchError, throwError, retry } from 'rxjs';
 
 import { APP_CONSTANTS } from '../constants/app.constants';
 import { AuthService } from '../services/auth/auth-service';
@@ -10,27 +10,23 @@ export const globalHttpErrorInterceptor: HttpInterceptorFn = (req, next) => {
   const errorHandler = inject(ErrorHandlerService);
   const authService = inject(AuthService);
   const MAX_RETRIES = APP_CONSTANTS.RETRY.COUNT;
-  const RETRY_DELAY_MS = APP_CONSTANTS.RETRY.DELAY_MS;
 
   return next(req).pipe(
     retry({
       count: MAX_RETRIES,
       delay: (error: HttpErrorResponse) => {
         if (error.status === 0 || error.status >= 500) {
-          return timer(RETRY_DELAY_MS);
+          throw error;
         }
         throw error;
-      },
-    }),
-    tap({
-      error: (error: HttpErrorResponse) => {
-        errorHandler.notifyError(error);
       },
     }),
     catchError((error: HttpErrorResponse) => {
       if (error.status === 401) {
         authService.logout();
       }
+
+      errorHandler.notifyError(error);
 
       return throwError(() => error);
     }),
