39b58eeff95e3bc407165778de563251914aebf5
Victor Bruce <victorbruce82@gmail.com>
2025-10-21 08:44:53 +0000

    chore(lint): update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -30,7 +30,88 @@ module.exports = tseslint.config(
           style: 'kebab-case',
         },
       ],
-      '@angular-eslint/prefer-on-push-component-change-detection': 'error',
+      complexity: 'off',
+      'max-classes-per-file': ['error', 1],
+      eqeqeq: ['error', 'always'],
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      '@typescript-eslint/switch-exhaustiveness-check': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'none',
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        {
+          accessibility: 'explicit',
+          ignoredMethodNames: [
+            'ngOnInit',
+            'ngOnDestroy',
+            'ngAfterViewInit',
+            'ngAfterContentInit',
+            'ngAfterViewChecked',
+            'ngAfterContentChecked',
+            'ngOnChanges',
+            'ngDoCheck',
+          ],
+          overrides: {
+            constructors: 'no-public',
+          },
+        },
+      ],
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: ['enumMember', 'typeLike'],
+          format: ['PascalCase'],
+          custom: {
+            regex: '(My|my)(?=[A-Z]\\w*)',
+            match: false,
+          },
+        },
+        {
+          selector: ['parameter'],
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: ['variable', 'function', 'method', 'classProperty', 'typeProperty'],
+          format: ['camelCase'],
+          custom: {
+            regex: '(My|my)(?=[A-Z]\\w*)',
+            match: false,
+          },
+        },
+        {
+          selector: ['variable'],
+          format: ['UPPER_CASE', 'camelCase'],
+          modifiers: ['global'],
+          custom: {
+            regex: '(My|my)(?=[A-Z]\\w*)',
+            match: false,
+          },
+        },
+        {
+          selector: ['variable'],
+          types: ['function'],
+          format: ['camelCase'],
+        },
+        {
+          selector: 'interface',
+          custom: {
+            regex: '[Ii](?=[A-Z]\\w*)',
+            match: false,
+          },
+          format: ['PascalCase'],
+        },
+      ],
+      'no-console': ['error', { allow: ['warn', 'error'] }],
     },
   },
   {
