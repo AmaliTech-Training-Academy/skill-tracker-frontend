9104f22cea5ce75abb4b7ce8688bb5a0d15147f7
Victor Bruce <victorbruce82@gmail.com>
2025-10-21 13:13:47 +0000

    chore(zone.js): add zone.js and remove provideZonelessChangeDetection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,4 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZonelessChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
@@ -12,7 +8,6 @@ import { authInterceptor, globalHttpErrorInterceptor } from './core';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor, globalHttpErrorInterceptor])),
   ],
