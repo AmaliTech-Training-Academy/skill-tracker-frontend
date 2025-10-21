fda21e11f8e5881a69110172c583d7654a784cae
Faisal <pros3sa@gmail.com>
2025-10-21 16:44:09 +0000

    fix: resolve provideHttpClient duplication error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 import { authInterceptor, globalHttpErrorInterceptor } from './core';
 
 export const appConfig: ApplicationConfig = {
