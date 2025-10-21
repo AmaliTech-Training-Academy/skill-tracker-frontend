0401e219747b050a09c68fa3c48be1b44ebe5e3a
Mildred Naab <mwinkomnaab@gmail.com>
2025-10-21 02:15:09 -1200

    chore(ngrx): add NgRx store, effects, entity, and devtools packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,16 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
+  isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { authInterceptor, globalHttpErrorInterceptor } from './core';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +19,8 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor, globalHttpErrorInterceptor])),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 };
