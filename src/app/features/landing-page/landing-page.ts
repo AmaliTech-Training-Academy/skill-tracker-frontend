cb4d199d0761926bc30a97b40874ef2bc1e12ae2
Mildred Naab <mwinkomnaab@gmail.com>
2025-10-22 06:36:09 -1200

    feat(landing): integrate child components into landing page template

--- src/app/features/landing-page/landing-page.ts
+++ src/app/features/landing-page/landing-page.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Features } from './components/features/features';
+import { HeroSection } from './components/hero-section/hero-section';
+import { SkillsDevelopment } from './components/skills-development/skills-development';
+import { LuminaCallout } from './components/lumina-callout/lumina-callout';
+import { PricingModels } from './components/pricing-models/pricing-models';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [Features, HeroSection, SkillsDevelopment, LuminaCallout, PricingModels],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss'
 })
