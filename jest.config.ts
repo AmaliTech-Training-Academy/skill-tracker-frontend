b431ac40e435a41f1256578f1537c24aaa98063c
Victor Bruce <victorbruce82@gmail.com>
2025-10-22 07:21:49 +0000

    chore(tooling): configure ESLint and Jest for TypeScript compatibility

--- jest.config.ts
+++ jest.config.ts
@@ -2,9 +2,12 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>\\src\\setup-jest.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
   moduleDirectories: ['node_modules', '<rootDir>'],
-  testMatch: ['**\\+(*.)+(spec).+(ts)'],
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+  },
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
   rootDir: process.cwd(),
 };
 
