c1b92c3d2b56a18555a5e65b09d99913e73ce5bd
Faisal <pros3sa@gmail.com>
2025-10-24 17:15:01 +0000

    refactor: hide footer on other pages

--- src/app/shared/compomonents/footer/footer.ts
+++ src/app/shared/compomonents/footer/footer.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
+  standalone: true
 })
-export class Footer {}
+export class Footer implements OnInit {
+  showFooter = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    
+    this.checkRoute(this.router.url);
+
+    
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.checkRoute(event.urlAfterRedirects);
+      });
+  }
+
+  private checkRoute(url: string) {
+    
+    this.showFooter = url === '/' || url === '';
+  }
+}
