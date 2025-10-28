514bc21af28ad23833d81c650531790e8c8ee50f
Mildred Naab <mwinkomnaab@gmail.com>
2025-10-28 01:01:13 -1200

    feat(lumina-callout): create accessible Lumina callout template

--- src/app/features/landing-page/components/lumina-callout/lumina-callout.ts
+++ src/app/features/landing-page/components/lumina-callout/lumina-callout.ts
@@ -7,5 +7,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LuminaCallout {
-
+  public title = 'Meet Lumina: The AI that illuminates your technical path.';
+  public description = 'Our built-in AI guide and tutor, Lumina, creates personalized challenges designed just for you, shedding light on complex concepts and helping you find your way when you get stuck.';
+  public buttonText = 'Meet Lumina';
+  public robotImageSrc = 'assets/robot.png';
+  public robotImageAlt = 'Lumina AI Robot';
 }
