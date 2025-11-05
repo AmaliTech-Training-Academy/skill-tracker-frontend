fb8d71bb2725e5e10a1d33495b997d4e8fdb6007
Mildred Naab <mwinkomnaab@gmail.com>
2025-11-04 20:52:17 -1200

    feat(tasks): create selectors for accessing and filtering tasks in store

---
+++ src/app/store/tasks/tasks.selectors.ts
@@ -0,0 +1,42 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { TasksState } from './tasks.state';
+import { Task } from '@app/core/models/tasks-model';
+
+export const selectTasksState = createFeatureSelector<TasksState>('tasks');
+
+export const selectTodayTasks = createSelector(selectTasksState, (state) => state.todayTasks);
+
+export const selectAllPreviousTasks = createSelector(
+  selectTasksState,
+  (state) => state.previousTasks,
+);
+
+export const selectSkillFilter = createSelector(selectTasksState, (state) => state.selectedSkill);
+
+export const selectTimeRangeFilter = createSelector(
+  selectTasksState,
+  (state) => state.selectedTimeRange,
+);
+
+export const selectFilteredTodayTasks = createSelector(
+  selectTodayTasks,
+  selectSkillFilter,
+  (tasks: Task[], skill: string) => {
+    if (skill === 'All') {
+      return tasks;
+    }
+    return tasks.filter((task) => task.skill === skill);
+  },
+);
+
+export const selectFilteredPreviousTasks = createSelector(
+  selectAllPreviousTasks,
+  selectSkillFilter,
+  selectTimeRangeFilter,
+  (tasks: Task[], skill: string, timeRange: string) => {
+    if (skill === 'All') {
+      return tasks;
+    }
+    return tasks.filter((task) => task.skill === skill);
+  },
+);
