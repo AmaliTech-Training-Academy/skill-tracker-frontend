4ab85c76de7cca4dcb88f8609d3cfab5634adb2c
Faisal <pros3sa@gmail.com>
2025-11-07 09:59:10 +0000

    test: add unit test

--- src/app/features/forgot-password/forgot-password.ts
+++ src/app/features/forgot-password/forgot-password.ts
@@ -1,44 +1,52 @@
-import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { 
+  Component, 
+  OnDestroy, 
+  ChangeDetectionStrategy, 
+  ChangeDetectorRef
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, takeUntil } from 'rxjs'; 
 import { InputFieldComponent } from '../../shared/input-field/input-field';
 import { getFormControl } from '@app/shared';
 import { Store } from '@ngrx/store';
 import * as AuthActions from '@app/store/auth/auth.actions';
 import { selectIsRequestingPasswordReset } from '@app/store/auth/auth.selectors';
-import { UserEmailRequest } from '@app/core';
-import { RouterLink } from '@angular/router';
+import { UserEmailRequest } from '@app/core'; 
+import { RouterLink } from '@angular/router'; 
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, InputFieldComponent, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, InputFieldComponent, RouterLink], 
   templateUrl: './forgot-password.html',
   styleUrls: ['./forgot-password.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ForgotPassword implements OnInit, OnDestroy {
-  public forgotPasswordForm!: FormGroup;
-  public loading$: Observable<boolean> = this.store.select(selectIsRequestingPasswordReset);
-
-  private readonly destroy$ = new Subject<void>();
-
+export class ForgotPassword implements OnDestroy {
+  
   constructor(
     private store: Store,
     private formBuilder: FormBuilder,
-  ) {}
-
-  ngOnInit(): void {
-    this.initForm();
+    private cdr: ChangeDetectorRef,
+  ) {
   }
 
-  private initForm(): void {
-    this.forgotPasswordForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-    });
-  }
+  public forgotPasswordForm: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
 
+  public loading$: Observable<boolean> = this.store.select(selectIsRequestingPasswordReset); 
+  private readonly destroy$ = new Subject<void>();
+
+  private readonly loadingSubscription = this.loading$.pipe(
+    takeUntil(this.destroy$)
+  ).subscribe((isLoading) => {
+    if (!isLoading) {
+        this.cdr.detectChanges(); 
+    }
+  });
+  
   public getControl = getFormControl;
 
   public submit(): void {
@@ -47,13 +55,15 @@ export class ForgotPassword implements OnInit, OnDestroy {
 
     const email = this.forgotPasswordForm.value.email as string;
     const request: UserEmailRequest = { email };
-    this.store.dispatch(AuthActions.forgotPassword({ request }));
-    this.getControl(this.forgotPasswordForm, 'email').reset('');
+    
+    this.store.dispatch(AuthActions.forgotPassword({ request })); 
+    this.getControl(this.forgotPasswordForm, 'email').reset(''); 
   }
 
   public tryDifferentEmail(): void {
     this.getControl(this.forgotPasswordForm, 'email').reset('');
     this.store.dispatch(AuthActions.resetPasswordResetState());
+    this.cdr.detectChanges(); 
   }
 
   ngOnDestroy(): void {
