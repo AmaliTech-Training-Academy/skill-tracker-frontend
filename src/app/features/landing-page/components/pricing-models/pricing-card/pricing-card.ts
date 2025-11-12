8285bfc9eba2adcd5be5d8c1f574a5e5754685e7
Mildred Naab <mwinkomnaab@gmail.com>
2025-11-11 22:54:00 -1200

    feat(pricing): add routing logic to navigate to specific plan confirmation pages

--- src/app/features/landing-page/components/pricing-models/pricing-card/pricing-card.ts
+++ src/app/features/landing-page/components/pricing-models/pricing-card/pricing-card.ts
@@ -1,10 +1,12 @@
 import { CurrencyPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Router } from '@angular/router';
+import { APP_CONSTANTS } from '@app/core/constants/app.constants';
 
 export interface PricingPlan {
   name: string;
   tagline: string;
-  price: number; 
+  price: number;
   period: string;
   isPopular: boolean;
   features: string[];
@@ -15,9 +17,32 @@ export interface PricingPlan {
   imports: [CurrencyPipe],
   templateUrl: './pricing-card.html',
   styleUrl: './pricing-card.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PricingCard {
-  public icon = 'assets/blue-checkmark.png'
+  constructor(private router: Router) {}
+
+  public icon = 'assets/blue-checkmark.png';
   public plan = input.required<PricingPlan>();
+
+  public onSelectPlan(): void {
+    const planName = this.plan().name;
+    let planId: number;
+
+    switch (planName) {
+      case 'Free Plan':
+        planId = 1;
+        break;
+      case 'Pro':
+        planId = 2;
+        break;
+      case 'Elite':
+        planId = 3;
+        break;
+      default:
+        planId = 1;
+    }
+
+    this.router.navigateByUrl(`${APP_CONSTANTS.FULL_PAGE_ROUTES.PLAN_CONFIRMATION}/${planId}`);
+  }
 }
