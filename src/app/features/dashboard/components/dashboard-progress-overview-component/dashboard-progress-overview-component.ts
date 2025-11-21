f530b9bed4f6a51acc56d662883229112d79a429
Victor Bruce <victorbruce82@gmail.com>
2025-11-21 06:28:53 +0000

    feat(progress-overview): connect progress overview section to live data

--- src/app/features/dashboard/components/dashboard-progress-overview-component/dashboard-progress-overview-component.ts
+++ src/app/features/dashboard/components/dashboard-progress-overview-component/dashboard-progress-overview-component.ts
@@ -1,14 +1,22 @@
 import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 import { ProgressBar, ProgressChart } from '@app/shared';
 import { CustomDropdown } from '@app/shared/components/custom-dropdown/custom-dropdown';
-import { AppError, SkillProgress } from '@app/core';
+import { AppError, SkillProgress, UserSelectedSkill } from '@app/core';
 import { CapitalizePipe } from '@app/shared/pipes/capitalize.pipe';
 import { ProgressBarSkeleton } from '../progress-bar-skeleton/progress-bar-skeleton';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-dashboard-progress-overview-component',
-  imports: [ProgressBar, ProgressChart, CustomDropdown, CapitalizePipe, ProgressBarSkeleton],
+  imports: [
+    ProgressBar,
+    ProgressChart,
+    CustomDropdown,
+    CapitalizePipe,
+    ProgressBarSkeleton,
+    FormsModule,
+  ],
   templateUrl: './dashboard-progress-overview-component.html',
   styleUrl: './dashboard-progress-overview-component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,25 +26,31 @@ export class DashboardProgressOverviewComponent {
   @Input({ required: true }) public progressChartData: { label: string; value: number }[] = [];
   @Input({ required: true }) public isDashboardAnalyticsLoading: boolean = false;
   @Input({ required: true }) public isDashboardAnalyticsError: AppError | null = null;
+  @Input() public isUserSkillsLoading: boolean = false;
 
   @Input({ required: true }) public selectedPeriod: string = 'weekly';
   @Output() public selectPeriod = new EventEmitter<string>();
+  @Output() public selectSkill = new EventEmitter<string>();
+  @Input() public selectedSkill: string | null = null;
+  @Input() public userSkills: UserSelectedSkill[] = [];
 
-  public data = {
-    progressChartData: {
-      weekly: [
-        { label: 'Mon', value: 30 },
-        { label: 'Tue', value: 10 },
-        { label: 'Wed', value: 5 },
-        { label: 'Thu', value: 0 },
-        { label: 'Fri', value: 0 },
-        { label: 'Sat', value: 0 },
-        { label: 'Sun', value: 0 },
-      ],
-    },
-  } as const;
+  public get skillOptions(): string[] {
+    return this.userSkills.map((skill) => skill.skillName);
+  }
+
+  public get selectedSkillName(): string {
+    const skill = this.userSkills.find((skill) => skill.skillId === this.selectedSkill);
+    return skill?.skillName || '';
+  }
 
   public onSelectPeriod(period: string): void {
     this.selectPeriod.emit(period);
+  }
+
+  public onSelectSkill(skillId: string): void {
+    const skill = this.userSkills.find((skill) => skill.skillId === skillId);
+    if (skill) {
+      this.selectSkill.emit(skill.skillId);
+    }
   }
 }
