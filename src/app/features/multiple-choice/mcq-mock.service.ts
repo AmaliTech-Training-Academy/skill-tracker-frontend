1a5c974836e0fcbb04ed173b2b880b01d7498e64
Faisal <pros3sa@gmail.com>
2025-11-21 06:18:20 +0000

    fix: resolve navigation error

--- src/app/features/multiple-choice/mcq-mock.service.ts
+++ src/app/features/multiple-choice/mcq-mock.service.ts
@@ -167,7 +167,7 @@ export class McqMockService {
 
     if (!questions) {
       this.toastService.showError('Quize Error', 'Task could not be found. Try again');
-      this.router.navigateByUrl('dashboard/tasks');
+      this.router.navigateByUrl('/dashboard/tasks');
 
       return of({
         success: false,
